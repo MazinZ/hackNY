670ed57e26eec5bc39a2d89debd51f994613c9cc
Mazin Zakaria <mazinzakaria1@gmail.com>
2015-09-26 16:26:11 -0400

    Finished object detection w/ webcam error

--- hackNYscript.cs
+++ hackNYscript.cs
@@ -1,15 +1,54 @@
 using UnityEngine;
 using System.Collections;
+using Vuforia;
 
-public class hackNYscript : MonoBehaviour {
+/* source: https://developer.vuforia.com/library/articles/Solution/How-To-Create-a-Simple-Cloud-Recognition-App-in-Unity */
 
-	// Use this for initialization
+public class hackNYscript : MonoBehaviour, ICloudRecoEventHandler {
+	private CloudRecoBehaviour mCloudRecoBehaviour;
+	private bool mIsScanning = false;
+	private string mTargetMetadata = "";
+
+
 	void Start () {
-	
+		mCloudRecoBehaviour = GetComponent<CloudRecoBehaviour>();
+		if (mCloudRecoBehaviour)
+		{
+			mCloudRecoBehaviour.RegisterEventHandler(this);
+		}
+	}
+
+	public void OnInitialized() {
+		Debug.Log ("Cloud Reco initialized");
+	}
+
+	public void OnInitError(TargetFinder.InitState initError) {
+		Debug.Log ("Cloud Reco init error " + initError.ToString());
+	}
+	public void OnUpdateError(TargetFinder.UpdateState updateError) {
+		Debug.Log ("Cloud Reco update error " + updateError.ToString());
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	public void OnStateChanged(bool scanning) {
+		mIsScanning = scanning;
+		if (scanning)
+		{
+			ObjectTracker tracker = TrackerManager.Instance.GetTracker<ObjectTracker>();
+			tracker.TargetFinder.ClearTrackables(false);
+		}
 	}
+
+
+	public void OnNewSearchResult(TargetFinder.TargetSearchResult targetSearchResult) {
+		mTargetMetadata = targetSearchResult.MetaData;
+		mCloudRecoBehaviour.CloudRecoEnabled = false;
+		Debug.LogError (mTargetMetadata);
+	}
+
+
 }
+
+
+
+
+
